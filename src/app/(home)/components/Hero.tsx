373c3d68d81f00a0f06aad7c147b3fb4b21e7367
Joel Silva <joelpitra44@gmail.com>
2025-05-07 14:31:10 +0100

    feat: header, and layout of landing page

---
+++ src/app/(home)/components/Hero.tsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+  return (
+    <section className="flex w-full items-center justify-center">
+      <div className="max-w-5xl w-full items-center justify-center flex flex-col"></div>
+    </section>
+  );
+}
--- src/app/ui/Header.tsx
+++ src/app/ui/Header.tsx
@@ -1,5 +1,52 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Header() {
-    return <header>
-        Forum header
+  const link = [
+    {
+      title: "Sobre",
+      id: "sobre",
+    },
+    {
+      title: "Investimento",
+      id: "investimento",
+    },
+    {
+      title: "Parceiros",
+      id: "parceiros",
+    },
+    {
+      title: "Contactos",
+      id: "contactos",
+    },
+  ];
+
+  return (
+    <header className="py-4 px-6 bg-primary w-full flex justify-center items-center">
+      <div className="max-w-5xl w-full flex justify-between items-center text-white">
+        <Link href={"/"}>
+          <Image
+            src={"/"}
+            alt={""}
+            width={44}
+            height={44}
+          />
+        </Link>
+
+        <ul className="flex gap-4 items-center">
+          {link.map((item, index) => (
+            <li key={index}>
+              <button className="font-semibold px-4 py-2">{item.title}</button>
+            </li>
+          ))}
+        </ul>
+
+        <Link
+          href={""}
+          className="bg-secondary px-4 py-2  font-semibold rounded-md">
+          Inscrever-se
+        </Link>{" "}
+      </div>
     </header>
+  );
 }
