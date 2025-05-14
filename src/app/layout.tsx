0cc23b54b6d9290c0e4786f17b025c79a5b95646
Joel Silva <joelpitra44@gmail.com>
2025-05-14 12:22:07 +0100

    feat: atualizar metadados e adicionar informações Open Graph para o Fórum sobre o impacto do setor financeiro

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,22 @@ const poppins = Poppins({
 
 export const metadata: Metadata = {
   title: "Fórum: O IMPACTO DO SECTOR FINANCEIRO NO CONTEÚDO LOCAL",
-  description: `O Fórum sobre "O Impacto do Setor Financeiro no Conteúdo Local" é um desdobramento do debate realizado durante a 5ª Plenária da 4ª Edição da Mesa Redonda com CEOs.`,
+  description: `Participe do Fórum sobre o impacto do setor financeiro no conteúdo local, uma extensão do debate realizado durante a 5ª Plenária da 4ª Edição da Mesa Redonda com CEOs. Descubra insights valiosos sobre o futuro do setor financeiro e sua relação com os negócios locais.`,
+  openGraph: {
+    type: "website",
+    title: "Fórum: O IMPACTO DO SECTOR FINANCEIRO NO CONTEÚDO LOCAL",
+    description: `Participe do Fórum sobre o impacto do setor financeiro no conteúdo local, uma extensão do debate realizado durante a 5ª Plenária da 4ª Edição da Mesa Redonda com CEOs. Entenda como o setor financeiro influencia os negócios locais.`,
+    url: "https://conteudolocal.globalsc.ao",
+    siteName: "Fórum Impacto Financeiro",
+    images: [
+      {
+        url: "/plenaria/1.JPG",
+        width: 1200,
+        height: 630,
+        alt: "Fórum sobre o impacto do setor financeiro no conteúdo local",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
