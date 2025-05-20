08f1d82719bd33139966db1da1e232860c362feb
Joel Silva <joelpitra44@gmail.com>
2025-05-20 17:09:37 +0100

    feat: corrigir estilos e textos no componente ContactUs; ajustar placeholders e classes

--- src/app/(home)/components/ContactUs.tsx
+++ src/app/(home)/components/ContactUs.tsx
@@ -63,7 +63,7 @@ export default function ContactUs({
       className="w-full bg-[url(/efeito-3.png)] bg-cover ">
       <div className="px-8 max-lg:p-0 py-12 max-lg:py-8 flex items-center justify-center bg-gradient-to-r from-secondary to-white/90 ">
         <div className="flex items-center max-lg:flex-col justify-center gap-12 max-lg:gap-8 w-full  max-w-5xl">
-          <div className="flex flex-col px-4 items-center justify-center text-center gap-4 w-full max-lg:w-full text-primary">
+          <div className="flex flex-col px-4 items-center justify-center text-center gap-4 w-full max-lg:w-full text-primary text-primary">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31541.415242441934!2d13.234995199999998!3d-8.8163926!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1a51f24e8277acab%3A0x8bbb561fd5396f18!2sEPIC%20SANA%20Luanda%20Hotel!5e0!3m2!1spt-PT!2sao!4v1747755552205!5m2!1spt-PT!2sao"
               allowFullScreen
@@ -74,9 +74,9 @@ export default function ContactUs({
 
           <form
             onSubmit={handleSubmit(onSubmit)}
-            className="bg-gray-200 text-primary p-8 flex flex-col items-center max-w-md w-full lg:rounded-lg gap-6">
+            className="bg-gray-200 text-primary text-primary p-8 flex flex-col items-center max-w-md w-full lg:rounded-lg gap-6">
             <h1 className="font-semibold text-center max-lg:text-lg">
-              ENTRE EM CONTACTO.
+              ENTRE EM CONTACTO
             </h1>
 
             <div className="w-full">
@@ -84,7 +84,7 @@ export default function ContactUs({
                 type="text"
                 placeholder="Nome completo"
                 {...register("name", { required: "Nome é obrigatório" })}
-                className={`w-full px-4 py-2 rounded-md outline-none bg-white/25 placeholder:text-primary/70 ${
+                className={`w-full px-4 py-2 rounded-md outline-none placeholder:italic bg-white/25 placeholdertext-gray-400/70 ${
                   errors.name && "focus:ring-red-500 focus:ring-1"
                 }`}
               />
@@ -110,7 +110,7 @@ export default function ContactUs({
                     message: "Email inválido",
                   },
                 })}
-                className={`w-full px-4 py-2 rounded-md outline-none bg-white/25 placeholder:text-primary/70 ${
+                className={`w-full px-4 py-2 rounded-md outline-none bg-white/25 placeholdertext-gray-400/70 ${
                   errors.email && "focus:ring-red-500 focus:ring-1"
                 }`}
               />
@@ -134,7 +134,7 @@ export default function ContactUs({
                 })}
                 className={`w-full px-4 py-2 ${
                   errors.contact && "focus:ring-red-500 focus:ring-1"
-                } rounded-md outline-none bg-white/25 placeholder:text-primary/70`}
+                } rounded-md outline-none bg-white/25 placeholdertext-gray-400/70`}
               />
               {errors.contact && (
                 <p
@@ -153,7 +153,7 @@ export default function ContactUs({
                 {...register("subject", {
                   required: "O assunto é obrigatório",
                 })}
-                className={`w-full px-4 py-2 rounded-md outline-none bg-white/25 placeholder:text-primary/70 ${
+                className={`w-full px-4 py-2 rounded-md outline-none bg-white/25 placeholdertext-gray-400/70 ${
                   errors.subject && "focus:ring-red-500 focus:ring-1"
                 }`}
                 rows={3}
@@ -171,7 +171,7 @@ export default function ContactUs({
 
             <button
               type="submit"
-              className="px-4 py-2 w-full rounded-md bg-primary text-white hover:bg-transparent hover:text-primary duration-300 transition-colors border border-secondary-blue cursor-pointer">
+              className="px-4 py-2 w-full rounded-md bg-primary text-white hover:bg-transparent hover:text-primaryduration-300 transition-colors border border-secondary-blue cursor-pointer">
               {isLoading ? (
                 <span className="animate-pulse">Enviando...</span>
               ) : (
